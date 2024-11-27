5cef3231df59b45660bb583ca97f68e9f14fddd2
chaseez <chasezundel@gmail.com>
2024-11-27 15:17:13 -0700

    Put comments for why I cleaned the dataset

--- clean-dataset.py
+++ clean-dataset.py
@@ -1,13 +1,20 @@
+"""
+The dataset had extra spaces between all the columns and it messed up how
+pandas read the csv, so instead of doing some hack parsing thing, I just
+wanted to clean the data to have it look nice.
+"""
+
 with open('OnlineNewsPopularity.csv') as f:
     lines = list(f.readlines())
 
 new_lines = []
 for line in lines:
+    # Remove the extra whitespace
     line = line.split()
+
+    # Add a whiteline for when writing to the csv
     new_lines.append(''.join(line) + '\n')
 
 with open('OnlineNewsPopularity.csv', 'w') as f:
     for line in new_lines:
         f.write(line)
-
-# git commit -m "Added dataset, description, and notebook"
